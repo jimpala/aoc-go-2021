489af5c94c909fad4d8ee937a0446c6c9138b111
James Pyne <james@pala.london>
2022-01-22 17:43:18 +0000

    Initial commit.

---
+++ cmd/main.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"aoc-2021/day01"
+)
+
+func main() {
+	day01.RunSolution()
+}
---
+++ day01/solution.go
@@ -0,0 +1,16 @@
+package day01
+
+import "fmt"
+
+type GroovyString string
+type Car struct {
+	topSpeed int64
+	colour   string
+}
+
+func RunSolution() {
+	fmt.Println("Hello")
+
+	foo := 2
+	fmt.Println(foo)
+}
